87d9a8865568eba1c9bea30e83b847e2bd37742f
agronomhidden <dasg@yandex.ru>
2020-01-16 14:12:58 +0300

    Update conf.go

--- config/conf.go
+++ config/conf.go
@@ -39,6 +39,33 @@ func ReadConfigFiles(path string) {
 	}
 }
 
+func ReadConfigFilesV2(path, file string) {
+		
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	exPath := filepath.Dir(ex)
+
+	viper.AddConfigPath(path)
+	viper.AddConfigPath(exPath)
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("common")
+	viper.ReadInConfig()
+
+	if file != "" {
+		file = path + file
+		var _, err = os.Stat(file)
+		if !os.IsNotExist(err) {
+			viper.SetConfigFile(file)
+			viper.MergeInConfig()
+		} else {
+			panic(fmt.Sprintf("Config file \"%s\" was not found", file))
+		}
+	}
+}
+
 func GetPort() string {
 	return viper.GetString("API.port")
 }
